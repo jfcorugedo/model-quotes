f9748ca0ddea234d621903f44848523aa9049791
jfcorugedo <jfcorugedo@gmail.com>
2019-03-24 18:15:21 +0100

    integrate DAO in resource layer, making the controller reactive and pass all the tests again

--- src/main/java/model/quotes/dao/QuoteMongoDAO.java
+++ src/main/java/model/quotes/dao/QuoteMongoDAO.java
@@ -4,13 +4,27 @@ import io.reactivex.Maybe;
 import model.quotes.rest.dto.Quote;
 
 import javax.inject.Singleton;
+import java.util.Arrays;
+import java.util.List;
 
 @Singleton
 public class QuoteMongoDAO implements QuoteDAO {
 
+    private static final List<Quote> quotes = Arrays.asList(
+        new Quote().setId(1L).setText("Don't leave for tomorrow what you can do today"),
+        new Quote().setId(2L).setText("At the beginning I was listening but..."),
+        new Quote().setId(3L).setText("A man has to do what a man has to do. - Conan the barbarian"),
+        new Quote().setId(4L).setText("Leave for tomorrow what you can do today because you might not have to do it at all")
+    );
+
+
     @Override
     public Maybe<Quote> findOne(Long id) {
-        return Maybe.empty();
+        return quotes
+            .stream()
+            .filter(quote -> quote.getId().equals(id))
+            .map(Maybe::just)
+            .findAny()
+            .orElse(Maybe.empty());
     }
-
 }
--- src/main/java/model/quotes/rest/QuoteResource.java
+++ src/main/java/model/quotes/rest/QuoteResource.java
@@ -2,32 +2,28 @@ package model.quotes.rest;
 
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
+import io.micronaut.http.MutableHttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
+import io.reactivex.Single;
+import lombok.RequiredArgsConstructor;
+import model.quotes.dao.QuoteDAO;
 import model.quotes.rest.dto.Quote;
 
-import java.util.Arrays;
-import java.util.List;
 
 @Controller("/quotes")
+@RequiredArgsConstructor
 public class QuoteResource {
 
-    private static final List<Quote> quotes = Arrays.asList(
-        new Quote().setId(1L).setText("Don't leave for tomorrow what you can do today"),
-        new Quote().setId(2L).setText("At the beginning I was listening but..."),
-        new Quote().setId(3L).setText("A man has to do what a man has to do. - Conan the barbarian"),
-        new Quote().setId(4L).setText("Leave for tomorrow what you can do today because you might not have to do it at all")
-    );
+    private final QuoteDAO quoteDAO;
 
     @Get(value = "{id}", produces = MediaType.APPLICATION_JSON)
-    public HttpResponse<Quote> getOne(@PathVariable("id") Long id) {
+    public Single<MutableHttpResponse<Quote>> getOne(@PathVariable("id") Long id) {
 
-        return quotes
-            .stream()
-            .filter(quote -> quote.getId().equals(id))
-            .findAny()
+        return quoteDAO
+            .findOne(id)
             .map(HttpResponse::ok)
-            .orElse(HttpResponse.notFound());
+            .toSingle(HttpResponse.notFound());
     }
 }
